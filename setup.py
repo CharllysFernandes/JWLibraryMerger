7ca83c3a8c42a7d07cd93202fc76318eb4123c2a
Charllys Fernandes <charllys.daiany@gmail.com>
2023-07-24 23:44:51 -0300

    setup.py create

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='JW_Merger_Playlist',    # Nome do pacote
-    version='0.1.0',       # Número de versão do pacote
-    packages=find_packages(),   # Lista de pacotes a serem incluídos
+    name='jw_library',
+    version='0.1.0',
+    packages=find_packages(),
     entry_points={
-        'console_scripts': ['jwlibrarymerger=jwlibrarymerger.main:main'],   # Script de entrada (opcional)
+        'console_scripts': ['merge_jwlibrary=main:main'],
     },
 )
